8805d6c72bec06d6b536109788ac512b5f228a9e
Jason Crawford <daxapps777@gmail.com>
2016-07-29 16:30:39 -0500

    Udemy

---
+++ MyMagicPill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMagicPill
+//
+//  Created by Jason Crawford on 7/29/16.
+//  Copyright © 2016 Dax Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
