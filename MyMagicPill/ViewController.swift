ff624b94c48976cbedc1498dce76aa4db25eed99
Jason Crawford <daxapps777@gmail.com>
2016-07-30 06:51:21 -0500

    I'm Learning iOS
    My first project where I finished the last half of the app on my own
    instead of copying.

--- MyMagicPill/ViewController.swift
+++ MyMagicPill/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryText: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var zipText: UITextField!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         
     }
 
@@ -20,6 +32,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryText.isHidden = true
+        zipLabel.isHidden = true
+        zipText.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryText.isHidden = false
+        zipLabel.isHidden = false
+        zipText.isHidden = false
+    }
 
+    @IBAction func buyBtnPressed(_ sender: AnyObject) {
+    }
 }
+
+
+
+
+
+
+
+
+
 
